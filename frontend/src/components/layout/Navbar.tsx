05882e62ce435b2504e090ab82d102648d97ce0f
Preyan Khasnobis <khasnobis.preyan888@gmail.com>
2026-03-17 10:47:22 +0530

    Comment out Home link in Navbar
    Comment out the Home link in the Navbar component.

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -13,12 +13,12 @@ export default function Navbar() {
             CheckMyMind.com
           </Link>
 
-          <Link
+          {/* <Link
             href="/"
             className="text-sm text-gray-500 hover:text-gray-700"
           >
             Home
-          </Link>
+          </Link> */}
 
         </div>
 
